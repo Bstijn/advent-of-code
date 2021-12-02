d0b3a09586701a4edf71a83a028dbba5c02288f4
Stijn <baltessen.stijn@gmail.com>
2021-12-02 22:08:30 +0100

    advent day one and two

---
+++ day-one/day-two/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using day_two;
+
+Console.WriteLine("Hello, World!");
+Class1 class1 = new Class1(); 
+foreach(var item in class1.Journey())
+{
+    Console.WriteLine(item.Key +":" +item.Value);
+}
